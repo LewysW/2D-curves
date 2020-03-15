059dd73faab0841565b67dba974ba076add1c13e
lewys <lewysward99@gmail.com>
2020-03-15 11:36:50 +0000

    Added comments to Curves.java, Bezier.java, BezierSpline.java

--- src/BezierSpline.java
+++ src/BezierSpline.java
@@ -1,61 +1,83 @@
 import java.awt.geom.Path2D;
 import java.awt.geom.Point2D;
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.LinkedList;
-import java.util.Queue;
 
+//Class to generate bezier spline
 public class BezierSpline {
+    //Represents number of points to draw and cubic
     private final int CUBIC = 4;
+    //Stores points on bezier spline
     private ArrayList<Point2D> spline = new ArrayList<>();
+    //Stores paths of each bezier curve
     private ArrayList<Path2D>  paths = new ArrayList<>();
 
-
+    //Clears points and path of bezier spline
     public void clear() {
         spline.clear();
         paths.clear();
     }
 
+    //Generates curve of bezier spline
     public void generateCurve(ArrayList<Point2D> points) {
         ArrayList<Point2D> splinePoints = new ArrayList<>();
+
+        //Add all plotted points to spline
         for (Point2D point2D : points) {
             Point2D point = new Point2D.Double();
             point.setLocation(point2D.getX(), point2D.getY());
             splinePoints.add(point);
         }
 
+        //Set start and end points of current bezier curve
         int start = 0;
         int end = start + 3;
 
+        //While the end of the current bezier curve is not the final plotted point
         while (end < splinePoints.size() - 1) {
+            //Add an imaginary point at the end index of the current curve (i.e. between the 3rd and 4th point of bezier)
             splinePoints.add(end, generateControlPoint(splinePoints.get(end - 1), splinePoints.get(end)));
 
+            //Update start and end points of current curve
             start = end;
             end = start + 3;
         }
 
+        //Assigns generated list of points to the bezier spline
         this.spline = splinePoints;
+
+        //Generate path through points
         generatePaths();
     }
 
+    //Generate list of paths, each passing through one of the generated bezier curves
     public void generatePaths() {
         ArrayList<Point2D> points = new ArrayList<>();
         int current = 0;
 
+        //While current point is not the end of the spline
         while (current != spline.size()) {
-            if (points.size() == 4) {
+            //If number of points traversed can produce a cubic curve
+            if (points.size() == CUBIC) {
+                //Create a new bezier curve
                 Bezier bezier = new Bezier();
+                //Generate a curve with the current list of points
                 bezier.generateCurve(points);
+                //Generate a path through the curve
                 bezier.generatePath();
+                //Add the path to the list of paths in the bezier spline
                 paths.add(bezier.getPath());
+                //Clear the list of points
                 points.clear();
+                //Decrement point by 1 (to include the current imaginary point in next bezier)
                 current--;
             } else {
+                //Otherwise add point to list of points and increment
                 points.add(spline.get(current++));
             }
             
         }
 
+        //Plot remaining points at end of spline (e.g. if 3 points left over for a quadratic curve)
         Bezier bezier = new Bezier();
         bezier.generateCurve(points);
         bezier.generatePath();
@@ -63,11 +85,13 @@ public class BezierSpline {
         points.clear();
     }
 
+    //Generates an imaginary control point halfway between two points
     private Point2D generateControlPoint(Point2D point1, Point2D point2) {
         return new Point2D.Double(((point2.getX() + point1.getX()) / 2.0),
                 ((point2.getY() + point1.getY()) / 2.0));
     }
 
+    //Getter for paths of bezier spline
     public ArrayList<Path2D> getPaths() {
         return paths;
     }
--- src/Curves.java
+++ src/Curves.java
@@ -8,19 +8,31 @@ import javax.swing.*;
 
 import static com.sun.java.accessibility.util.AWTEventMonitor.addWindowListener;
 
+/**
+ * Class to manage the swing user interface
+ */
 public class Curves extends JPanel {
+    //Stores the list of points entered by the user
     private ArrayList<Point2D> points = new ArrayList<>();
+
+    //Creates the instances of the three class which generate the specified curves
     private Bezier bezier = new Bezier();
     private BezierSpline bezierSpline = new BezierSpline();
     private ICPS ICPS = new ICPS();
 
-    private final int BORDER_GAP = 30;
+    //JFrame representing the GUI window
     private JFrame frame;
+    //Panel to place buttons and checkboxes on
     private JPanel panel;
+
+    //Button to clear the display
     private JButton clear = new JButton("Clear");
+    //Button to draw the curves on the display
     private JButton draw = new JButton("Draw");
+    //Button to generate a point on the bezier curve, then draw a tangent and perpendicular line
     private JButton gen = new JButton("Generate");
 
+    //Check boxes for each curve to allow the user to toggle that curve on or off
     static private JCheckBox bezierCheck = new JCheckBox("Bezier");
     static private JCheckBox bezierSplineCheck = new JCheckBox("Bezier Spline");
     static private JCheckBox ICPSCheck = new JCheckBox("Interpolating Cubic Polynomial Spline");
@@ -29,41 +41,52 @@ public class Curves extends JPanel {
         Curves curves = new Curves();
         curves.frame = new JFrame();
 
+        //Initialise panel to place buttons on
         curves.panel = new JPanel();
         curves.panel.setLayout(new BoxLayout(curves.panel, BoxLayout.PAGE_AXIS));
 
+        //Add buttons to panel
         curves.panel.add(curves.clear);
         curves.panel.add(curves.draw);
         curves.panel.add(curves.gen);
 
+        //Add check boxes to panel
         curves.panel.add(bezierCheck);
         curves.panel.add(bezierSplineCheck);
         curves.panel.add(ICPSCheck);
 
-
-        curves.panel.updateUI();
-
+        //Specifies layout of frame
         curves.frame.setTitle("Curves in Computer Graphics");
         curves.frame.setSize(1280, 720);
         Container contentPane = curves.frame.getContentPane();
         contentPane.add(curves, BorderLayout.CENTER);
+
+        //Specifies border of panel
         curves.panel.setBorder(BorderFactory.createLineBorder(Color.gray));
+        //Adds panel to frame
         contentPane.add(curves.panel, BorderLayout.LINE_START);
+        //Sets frame to visible
         curves.frame.setVisible(true);
 
+        //Draws the frame
         curves.repaint();
+
+        //Ensures that program exits when window is closed
         curves.frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        //Sets resizable to false for the window
         curves.frame.setResizable(false);
     }
 
 
     public Curves() {
+        //Exits program on window being closed
         addWindowListener(new WindowAdapter() {
             public void windowClosing(WindowEvent e) {
                 System.exit(0);
             }
         });
 
+        //Adds points to the display when clicked
         addMouseListener(new MouseAdapter() {// provides empty implementation of all
             // MouseListener`s methods, allowing us to
             // override only those which interests us
@@ -74,30 +97,39 @@ public class Curves extends JPanel {
             }
         });
 
+        //Clears the curves and plotted points from the display and repaints
         clear.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent actionEvent) {
                 //Removes points from array list points
                 points.clear();
+                //Clears bezier curve
                 bezier.clear();
+                //Clears bezier spline
                 bezierSpline.clear();
+                //Clears interpolating cubic polynomial spline
                 ICPS.clear();
+                //Sets random point plotted on bezier to null
                 bezier.setPoint(null);
                 repaint();
             }
         });
 
+        //Draws the curves on the display
         draw.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent actionEvent) {
+                //If 1 or fewer points, display nothing
                 if (points.size() <= 1) return;
 
-                //Removes points from array list points
+                //Clears bezier curve and point and generates new curve
                 bezier.clear();
                 bezier.setPoint(null);
                 bezier.generateCurve(points);
 
+                //Clears bezier spline and generates new curve
                 bezierSpline.clear();
                 bezierSpline.generateCurve(points);
 
+                //Clears interpolating cubic polynomial spline and generates new curve
                 ICPS.clear();
                 ICPS.generateCurve(points);
 
@@ -106,21 +138,28 @@ public class Curves extends JPanel {
             }
         });
 
+        //Generates the random point on the bezier curve
         gen.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent actionEvent) {
+                //Do nothing if 1 or less points have been plotted
                 if (bezier.getCurve().size() <= 1) return;
 
+                //Reset the tangent and normal of bezier curve
                 bezier.resetTangent();
                 bezier.resetNormal();
 
+                //If the bezier curve has been toggled on
                 if (bezierCheck.isSelected()) {
+                    //Generate a random point
                     Random random = new Random();
                     double randomDouble = random.nextDouble();
                     int index = (int) (bezier.getCurve().size() * randomDouble);
-
                     bezier.setPoint(bezier.getCurve().get(index));
+
+                    //Get vector of tangent to bezier curve using bezier function derivative
                     Point2D vector = bezier.derivative(index * (1.0 / bezier.getCurve().size()));
 
+                    //Generates tangent and perpendicular line to bezier curve
                     bezier.generateTangent(vector);
                     bezier.generateNormal(vector);
 
@@ -130,12 +169,14 @@ public class Curves extends JPanel {
         });
     }
 
+    //Paints the display
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         Graphics2D graphics2D = (Graphics2D) g;
 
-        //Clears points if list of points are empty
+        //If no points have been plotted
         if (points.isEmpty()) {
+            //clear display and draw set of axes
             clearDisplay(g);
             g.setColor(Color.BLACK);
             g.drawLine(0, getHeight()/2, getWidth(), getHeight() / 2);
@@ -144,52 +185,67 @@ public class Curves extends JPanel {
         }
 
 
-        //Plots point where user has clicked
+        //Plots points where user has clicked
         for (Point2D p : points) {
             graphics2D.setColor(Color.BLACK);
             graphics2D.fillOval((int) (p.getX() - 5.0 / 2.0), (int) (p.getY() - 5.0 / 2.0), 5, 5);
         }
 
+        //If bezier spline has been toggled on
         if (bezierSplineCheck.isSelected()) {
-            //Draws bezier spline
+            //Set colour to green
             graphics2D.setColor(Color.GREEN);
 
+            //Draw bezier spline
             for (Path2D path2D : bezierSpline.getPaths()) {
                 graphics2D.draw(path2D);
             }
         }
 
-
+        //If bezier curve has been toggled on
         if (bezierCheck.isSelected()) {
-            //Draws bezier curve
+            //Set colour to red
             graphics2D.setColor(Color.RED);
+            //Draw bezier curve
             graphics2D.draw(bezier.getPath());
 
+            //If random point has been generated
             if (bezier.getPoint() != null) {
+                //Set colour to black
                 g.setColor(Color.BLACK);
+                //Draw oval to mark random point
                 graphics2D.drawOval((int) ((bezier.getPoint().getX() - 5.0 / 2.0) - 7.5),
                         (int) ((bezier.getPoint().getY() - 5.0 / 2.0) - 7.5), 20, 20);
 
+                //Set colour to orange
                 g.setColor(Color.ORANGE);
+                //Draw tangent to bezier curve at random point
                 graphics2D.draw(bezier.getTangent());
+                //Set colour to magenta
                 g.setColor(Color.magenta);
+                //Draw perpendicular line to bezier curve
                 graphics2D.draw(bezier.getNormal());
             }
         }
 
+        //If interpolating cubic polynomial spline has been toggled on
         if (ICPSCheck.isSelected()) {
-            //Draws interpolating cubic polynomial spline
+            //Set colour to blue
             graphics2D.setColor(Color.BLUE);
+            //Draw interpolating cubic polynomial spline
             graphics2D.draw(ICPS.getPath());
         }
 
+        //Sets colour to black
         g.setColor(Color.BLACK);
+        //Draw set of axes
         g.drawLine(0, getHeight()/2, getWidth(), getHeight() / 2);
         g.drawLine(getWidth() / 2, 0, getWidth() / 2, getHeight());
 
     }
 
-    public void clearDisplay(Graphics g) {
+    //Clears the display
+    private void clearDisplay(Graphics g) {
         //Clears points by setting display to the default background colour
         g.setColor(frame.getBackground());
         g.fillRect(0, 0, getWidth(), getHeight());
